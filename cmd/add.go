e3c8fd195e126c5846c238856d5db78f85cdaf20
Isaac Vargas <hello@isaacvargas.dev>
2026-09-08 08:52:18 -0700

    feat: added shortcuts for install types

--- cmd/add.go
+++ cmd/add.go
@@ -8,9 +8,14 @@ import (
 )
 
 var (
-	installType string
-	group string
+	installType    string
+	group          string
 	installCommand string
+	isPacman       bool
+	isAUR          bool
+	isFlatpak      bool
+	isOmarchy      bool
+	isMise         bool
 )
 
 // groupCmd represents the group command
@@ -33,10 +38,29 @@ omarchy plugin. If ommitted, the install command will equal the
 name of the package.
 
 `,
-	Args:  cobra.ExactArgs(1),
+	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		// name of the entry
 		name := args[0]
 
+		switch {
+		case isPacman:
+			installType = "pacman"
+		case isAUR:
+			installType = "aur"
+		case isFlatpak:
+			installType = "flatpak"
+		case isOmarchy:
+			installType = "omarchy"
+		case isMise:
+			installType = "mise"
+		}
+
+		if installType == "" {
+			return fmt.Errorf("an install type must be specified via -i or --installType or a shortcut flag (-p, -a, -f, -o, -m)")
+		}
+
+		// if install command not included this will equal to the name
 		hasInstallCommand := installCommand != ""
 		ic := name
 		if hasInstallCommand {
@@ -67,8 +91,7 @@ name of the package.
 			return fmt.Errorf("saving manifest entry: %w", err)
 		}
 
-
-		fmt.Printf("Created entry %q in group %q for %q \n", name, group, installType )
+		fmt.Printf("Created entry %q in group %q for %q \n", name, group, installType)
 
 		return nil
 	},
@@ -78,6 +101,15 @@ func init() {
 	rootCmd.AddCommand(addCmd)
 	addCmd.Flags().StringVarP(&group, "group", "g", "all", "specifies the group to add this entry to")
 	addCmd.Flags().StringVarP(&installType, "installType", "i", "", "type of entry")
-	_ = addCmd.MarkFlagRequired("installType")
 	addCmd.Flags().StringVarP(&installCommand, "installCommand", "c", "", "simply the name of packages for packages or the full command for something like flatpak")
+
+	// install type boolean flags for shortcuts
+	addCmd.Flags().BoolVarP(&isPacman, "pacman", "p", false, "shortcut for pacman install type")
+	addCmd.Flags().BoolVarP(&isAUR, "aur", "a", false, "shortcut for aur install type")
+	addCmd.Flags().BoolVarP(&isFlatpak, "flatpak", "f", false, "shortcut for flatpak install type")
+	addCmd.Flags().BoolVarP(&isOmarchy, "omarchy", "o", false, "shortcut for omarchy install type")
+	addCmd.Flags().BoolVarP(&isMise, "mise", "m", false, "shortcut for mise install type")
+
+	// ensure that only one shortcut or the install command provided
+	addCmd.MarkFlagsMutuallyExclusive("installType", "pacman", "aur", "flatpak", "omarchy", "mise")
 }
